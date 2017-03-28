1f068334535c20ef11f82b48020d9dbe3659b943
Jakob Benner <hilltopper123@yahoo.com>
2017-03-28 13:13:59 -0500

    Add files via upload

--- Joe src/Battlefield.java
+++ Joe src/Battlefield.java
@@ -1,7 +1,20 @@
-package com.center.pokemon.desktop;
+package classes;
 
 public class Battlefield {
     private Pokemon[] opponentParty;
+
+    private boolean endturn;
+    private int turncount;
+
+    //when pokemon completes an action in battle, endturn becomes true to keep the order of the battle turncount goes up one
+    //turncount is counter which tracks how many turns it has completed
+    //the pokemon with the lowest turn count will go first, and if both pokemon have the same turncount, the first pokemon goes  m
+    public int turnOrder(boolean endturn, int turncount){
+
+    }
+
+
+
 
     public Battlefield(String[][] oParty){
         opponentParty = new Pokemon[oParty.length];
@@ -9,5 +22,7 @@ public class Battlefield {
             opponentParty[i] = new Pokemon(oParty[i][0], Integer.valueOf(oParty[i][1]));
         }
     }
-    
+
+
+
 }
